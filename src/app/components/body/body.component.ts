d526271690f6f48e4a77f6de8ab3566abacda8f4
victor mican <victormican@gmail.com>
2023-05-21 13:59:07 -0500

    Initial commit4

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  //template:`<h1>Hola soy el componente</h1> 
+    //        <h1>Prueba</h1> 
+  //`,
+  
+  templateUrl: './body.component.html'
+})
+
+export class BodyComponent {
+    
+}
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  //template:`<h1>Hola soy el componente</h1> 
+    //        <h1>Prueba</h1> 
+  //`,
+  
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+    
+}
